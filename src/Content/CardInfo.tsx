adccb7595376c0f871be857786946275f64ff4a5
mmc-local <MMC_SOFTWARE@bitbucket.org>
2023-02-24 00:04:06 +0500

    card section fixed

---
+++ src/Content/CardInfo.tsx
@@ -0,0 +1,27 @@
+import dayInMetaVerse from "../public/dayInMetaVerse.jpg";
+import MoneyInMetaverse from "../public/Money_in_Metaverse.jpg";
+import outcome from "../public/outcome.jpg";
+
+
+export const Data = [
+  {
+    title: "Web3 and Metaverse technologies",
+    image: dayInMetaVerse,
+    text: `The internet is without a doubt the most important technological
+        development in human history. Web3 and metaverse technologies expand
+        the internet as we know it by introducing novel features and
+        advancements. Metaverse will make use of all aspects of modern
+        technology, including 3D, VR, AR, AI, blockchain, cloud computing,
+        voice computing, ambient computing, and more.`,
+  },
+  {
+    title: "Earn While You Learn",
+    image: MoneyInMetaverse,
+    text: `In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the program’s beginning. It resembles a cross between a corporate venture and an educational project.`,
+  },
+  {
+    title: "The Outcome for Participants of the Program",
+    image: outcome,
+    text: `In this brand-new type of curriculum, students will learn how to make money and boost exports in the classroom and will begin doing so within six months of the program’s beginning. It resembles a cross between a corporate venture and an educational project.`,
+  },
+];
--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,48 +1,120 @@
 import { Box, Flex, Heading, Text, VStack } from "@chakra-ui/react";
 import NextImage from "next/image";
-import dayInMetaVerse from "../public/dayInMetaVerse.jpg";
 import { motion } from "framer-motion";
 
-export default function Info() {
+import { Data } from "../Content/CardInfo";
+
+const MyCard = ({ _title, _text, _image }) => {
   const AnimatedBox = motion(Flex);
-
   return (
-    <Flex mx={["10", "10", "10", "40"]} my="8">
-      <Flex gap={[5, 5, 10, 10]} alignItems="center" direction={["column", "column", "row", "row"]} >
-        <Flex h={[200, 200, 400, 400]} w={["70", "80", "auto", "auto"]}>
-          <AnimatedBox
-
-            whileHover={{
-              scale: [1, 1.1],
-              transition: { duration: 0.5 },
-            }}
-            transition={{
-              duration: 0.4,
-              ease: "easeInOut",
-            }}
-          >
-            <NextImage
-              style={{ borderRadius: "25px" }}
-              src={dayInMetaVerse}
-              alt="metaverse image"
-            />
-          </AnimatedBox>
-        </Flex>
-        <Flex direction={["column"]} gap={["2", "2", "5", "5"]} justifyContent={"flex-start"}>
-          <Heading fontFamily={"sans-serif"} fontSize={["2xl", "2xl", "4xl", "4xl"]}>
-            Web3 and Metaverse technologies
-          </Heading>
-          <Text fontFamily={"sans-serif"} fontSize={["md", "md", "lg", "xl"]}>
-            The internet is without a doubt the most important technological
-            development in human history. Web3 and metaverse technologies expand
-            the internet as we know it by introducing novel features and
-            advancements. Metaverse will make use of all aspects of modern
-            technology, including 3D, VR, AR, AI, blockchain, cloud computing,
-            voice computing, ambient computing, and more.
-          </Text>
-        </Flex>
+    <Flex
+      gap={[5, 5, 10, 10]}
+      alignItems="center"
+      direction={["column", "column", "row", "row"]}
+    >
+      <Flex h={[200, 200, 400, 400]} w={["70", "80", "auto", "auto"]}>
+        <AnimatedBox
+          whileHover={{
+            scale: [1, 1.1],
+            transition: { duration: 0.5 },
+          }}
+          transition={{
+            duration: 0.4,
+            ease: "easeInOut",
+          }}
+        >
+          <NextImage
+            style={{ borderRadius: "25px" }}
+            src={_image}
+            alt="metaverse image"
+          />
+        </AnimatedBox>
+      </Flex>
+      <Flex
+        direction={["column"]}
+        gap={["2", "2", "5", "5"]}
+        justifyContent={"flex-start"}
+      >
+        <Heading
+          fontFamily={"sans-serif"}
+          fontSize={["2xl", "2xl", "4xl", "4xl"]}
+        >
+          {_title}
+        </Heading>
+        <Text fontFamily={"sans-serif"} fontSize={["md", "md", "lg", "xl"]}>
+          {_text}
+        </Text>
       </Flex>
     </Flex>
+  );
+};
 
+const MyCardReverse = ({ _title, _text, _image }) => {
+  const AnimatedBox = motion(Flex);
+  return (
+    <Flex
+      gap={[5, 5, 10, 10]}
+      alignItems="center"
+      flexDir={[
+        "column-reverse",
+        "column-reverse",
+        // "row-reverse",
+        // "row-reverse",
+      ]}
+      direction={["column", "column", "row", "row"]}
+    >
+      <Flex
+        direction={["column"]}
+        gap={["2", "2", "5", "5"]}
+        justifyContent={"flex-start"}
+      >
+        <Heading
+          fontFamily={"sans-serif"}
+          fontSize={["2xl", "2xl", "4xl", "4xl"]}
+        >
+          {_title}
+        </Heading>
+        <Text fontFamily={"sans-serif"} fontSize={["md", "md", "lg", "xl"]}>
+          {_text}
+        </Text>
+      </Flex>
+
+      <Flex h={[200, 200, 400, 400]} w={["70", "80", "auto", "auto"]}>
+        <AnimatedBox
+          whileHover={{
+            scale: [1, 1.1],
+            transition: { duration: 0.5 },
+          }}
+          transition={{
+            duration: 0.4,
+            ease: "easeInOut",
+          }}
+        >
+          <NextImage
+            style={{ borderRadius: "25px" }}
+            src={_image}
+            alt="metaverse image"
+          />
+        </AnimatedBox>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default function Info() {
+  return (
+    <Flex mx={["10", "10", "10", "40"]} gap="10" direction="column" my="8">
+      {Data.map((item, index) => {
+        return index % 2 == 0 ? (
+          <MyCard _title={item.title} _text={item.text} _image={item.image} />
+        ) : (
+          <MyCardReverse
+            _title={item.title}
+            _text={item.text}
+            _image={item.image}
+          />
+        );
+      })}
+    </Flex>
   );
 }
